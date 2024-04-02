2395161edc9abb6459721dafc1d397218f5ea6b2
adamvnovak <adamvnovak@protonmail.com>
2024-04-02 15:25:54 -0400

    email

--- Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Utils/Validate.swift
+++ Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Utils/Validate.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct Validate {
     
     //TODO: display notice during auth that they can only use these characters
-    static func validateUsername(_ username: String) -> Bool {
+    static public func validateUsername(_ username: String) -> Bool {
         var usernamePermittedCharacters: CharacterSet = [".", "_"]
         usernamePermittedCharacters.formUnion(.alphanumerics)
         let isValidUsername = usernamePermittedCharacters.isSuperset(of: CharacterSet(charactersIn: username)) && username.count > 3 && username.count <= 25
@@ -18,17 +18,17 @@ public struct Validate {
         return isValidUsername
     }
     
-    static func validateName(_ name: String) -> Bool {
+    static public func validateName(_ name: String) -> Bool {
         let namePermittedCharacters: CharacterSet = .letters
         let isValidName = namePermittedCharacters.isSuperset(of: CharacterSet(charactersIn: name)) && name.count > 0 && name.count < 20
         return isValidName
     }
     
-    static func validateSex(_ sex: String) -> Bool {
+    static public func validateSex(_ sex: String) -> Bool {
         return sex != "" && sex != "blank"
     }
     
-    static func validateEmail(_ email: String) -> Bool {
+    static public func validateEmail(_ email: String) -> Bool {
         let regex = try! NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: [.caseInsensitive])
         return regex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.count)) != nil
     }
