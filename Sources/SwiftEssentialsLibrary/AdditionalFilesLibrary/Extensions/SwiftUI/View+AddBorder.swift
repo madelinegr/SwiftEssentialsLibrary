b2717d37d264ba940e4bf15644baa96a627c1dd3
adamvnovak <adamvnovak@protonmail.com>
2024-04-15 17:48:14 -0400

    camera issue fixed

---
+++ Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/SwiftUI/View+AddBorder.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Novak on 4/13/24.
+//
+
+import SwiftUI
+
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
