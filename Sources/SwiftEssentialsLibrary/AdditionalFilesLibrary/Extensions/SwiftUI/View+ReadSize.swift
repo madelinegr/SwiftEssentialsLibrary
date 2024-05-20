0d43e130c7115552bbfadd7eecb790a005b35f68
adamvnovak <adamvnovak@protonmail.com>
2024-05-20 14:40:22 -0400

    major ui progress in curate my night

--- Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/SwiftUI/View+ReadSize.swift
+++ Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/SwiftUI/View+ReadSize.swift
@@ -23,3 +23,20 @@ private struct SizePreferenceKey: PreferenceKey {
   static var defaultValue: CGSize = .zero
   static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
 }
+
+public extension View {
+  func readFrameGlobal(onChange: @escaping (CGRect) -> Void) -> some View {
+    background(
+      GeometryReader { geometryProxy in
+        Color.clear
+              .preference(key: FrameGlobalPreferenceKey.self, value: geometryProxy.frame(in: .global))
+      }
+    )
+    .onPreferenceChange(FrameGlobalPreferenceKey.self, perform: onChange)
+  }
+}
+
+private struct FrameGlobalPreferenceKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {}
+}
