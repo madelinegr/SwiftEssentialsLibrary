d91aa52b90f769149a5b2a15ad8527b526ee0c47
adamvnovak <adamvnovak@protonmail.com>
2024-06-06 20:28:23 -0400

    fix current event indexing issue

--- Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/UIKit/UIView/UIView+TopBorder.swift
+++ Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/UIKit/UIView/UIView+TopBorder.swift
@@ -62,22 +62,27 @@ public extension UIView {
 public extension UIView {
     func addTopBorder(with color: UIColor?, andWidth borderWidth: CGFloat) {
         let border = UIView()
+        border.tag = 44
         border.backgroundColor = color
         border.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
         border.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: borderWidth)
         addSubview(border)
     }
 
-    func addBottomBorder(with color: UIColor?, andWidth borderWidth: CGFloat) {
+    @discardableResult
+    func addBottomBorder(with color: UIColor?, andWidth borderWidth: CGFloat) -> UIView {
         let border = UIView()
+        border.tag = 43
         border.backgroundColor = color
         border.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
         border.frame = CGRect(x: 0, y: frame.size.height - borderWidth, width: frame.size.width, height: borderWidth)
         addSubview(border)
+        return border
     }
 
     func addLeftBorder(with color: UIColor?, andWidth borderWidth: CGFloat) {
         let border = UIView()
+        border.tag = 42
         border.backgroundColor = color
         border.frame = CGRect(x: 0, y: 0, width: borderWidth, height: frame.size.height)
         border.autoresizingMask = [.flexibleHeight, .flexibleRightMargin]
@@ -86,6 +91,7 @@ public extension UIView {
 
     func addRightBorder(with color: UIColor?, andWidth borderWidth: CGFloat) {
         let border = UIView()
+        border.tag = 41
         border.backgroundColor = color
         border.autoresizingMask = [.flexibleHeight, .flexibleLeftMargin]
         border.frame = CGRect(x: frame.size.width - borderWidth, y: 0, width: borderWidth, height: frame.size.height)
