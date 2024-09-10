1b13a8c5ccb21037bd67dc8c9c262e9e8d0f7739
adamvnovak <adamvnovak@protonmail.com>
2024-09-10 09:10:38 -0400

    remove tiktok bc it's bugging

--- Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/UIKit/UIViewController/UIVC+InteractivePop.swift
+++ Sources/SwiftEssentialsLibrary/AdditionalFilesLibrary/Extensions/UIKit/UIViewController/UIVC+InteractivePop.swift
@@ -10,15 +10,17 @@ import UIKit
 extension UIViewController: UIGestureRecognizerDelegate {
 
     public func disableInteractivePopGesture() {
-    navigationItem.hidesBackButton = true
-    navigationController?.interactivePopGestureRecognizer?.delegate = self
-    navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        navigationItem.hidesBackButton = true
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
   }
 
+    //this was working at some point, but it's not consistently working for me anymore.
     public func enableInteractivePopGesture() {
-    navigationItem.hidesBackButton = false
-    navigationController?.interactivePopGestureRecognizer?.delegate = self
-    navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        navigationItem.hidesBackButton = false
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+//        navigationController?.interactivePopGestureRecognizer?.delegate = nil //only works when doing this
   }
     
 }
